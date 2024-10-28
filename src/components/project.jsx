0c358d448791cca9fb963285d78adff8b6e57f4a
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-28 10:43:10 +0700

    feat: fix margins on cards

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -3,17 +3,17 @@ function Project() {
   return (
     <div className="w-full mt-5">
         <h1 className="font-fira font-bold text-2xl underline underline-offset-2">Project:</h1>
-        <div className="card filter backdrop:blur-lg w-full shadow-md mt-2">
+        <div className="card filter backdrop:blur-sm w-full shadow-md">
   <div className="card-body">
-    <h2 className="card-title cursor-pointer">Dimas yoga</h2>
+    <h2 className="card-title cursor-pointer underline text-sky-400 underline-offset-2">Dimas yoga</h2>
     <p>Personal web portfolio that I made using ReactJS</p>
     <div className="card-actions justify-end">
     </div>
   </div>
 </div>
-<div className="card filter backdrop:blur-xl w-full shadow-md mt-2">
+<div className="card filter backdrop:blur-xl w-full shadow-md">
   <div className="card-body">
-    <h2 className="card-title cursor-pointer">Quranku</h2>
+    <h2 className="card-title cursor-pointer underline underline-offset-2 text-sky-400">Quranku</h2>
     <p>a website that I developed only with HTML, CSS and JavaScript</p>
     <div className="card-actions justify-end">
     </div>
