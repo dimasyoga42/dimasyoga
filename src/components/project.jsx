81edb3bb57f7fdafe30d8a13dfd66454e82ae967
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-28 13:37:45 +0700

    style: change the card design

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,25 +1,29 @@
 
 function Project() {
   return (
-    <div className="w-full mt-5">
-        <h1 className="font-fira font-bold text-2xl underline underline-offset-2">Project:</h1>
-        <div className="card filter backdrop:blur-sm w-full shadow-md">
-  <div className="card-body">
-    <h2 className="card-title cursor-pointer underline text-sky-400 underline-offset-2">Dimas yoga</h2>
-    <p>Personal web portfolio that I made using ReactJS</p>
+    <>
+    <div className="mt-9">
+    <h1 className="mt-2 font-fira font-bold text-2xl text-sky-400 underline underline-offset-2">Project</h1>
+    <small className="text-gray-400 italic font-fira">some things I ve been working on</small>
+    </div>
+    <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-2 ">
+    <div className="rounded-lg p-4 text-accent shadow-md transition-shadow duration-300 hover:shadow-lg h-32 ">
+  <div className="mt-5">
+    <h2 className="text-xl font-bold cursor-pointer underline underline-offset-2 text-sky-400">Quranku</h2>
+    <p className="text-gray-400 text-xs">a website that I developed only with HTML, CSS and JavaScript</p>
     <div className="card-actions justify-end">
     </div>
   </div>
 </div>
-<div className="card filter backdrop:blur-xl w-full shadow-md">
-  <div className="card-body">
-    <h2 className="card-title cursor-pointer underline underline-offset-2 text-sky-400">Quranku</h2>
-    <p>a website that I developed only with HTML, CSS and JavaScript</p>
-    <div className="card-actions justify-end">
-    </div>
+
+<div className="rounded-lg p-4 text-accent shadow-md transition-shadow duration-300 hover:shadow-lg h-32">
+  <div className=" mt-5">
+    <h2 className="font-bold text-xl cursor-pointer underline underline-offset-2 text-sky-400">Quranku</h2>
+    <p className="text-gray-400 text-xs">a website that I developed only with HTML, CSS and JavaScript</p>
   </div>
 </div>
-    </div>
+</div>
+    </>
   )
 }
 
