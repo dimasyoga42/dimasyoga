56f51b9d67dc280f084401710c31e373c359dade
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-28 10:44:28 +0700

    adding components

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,16 +2,16 @@ import Medsos from "../components/medsos"
 import Project from "../components/project"
 function Home() {
   return (
-    <div className="font-fira mt-5 ml-2">
+    <div className="font-fira mt-5 ml-2 min-h-screen">
       <h1 className="font-bold text-2xl">hi, Im</h1>
       <div className="flex justify-items-center items-center">
-      <small className="text-gray-400">(ID)</small><h1 className="text-2xl font-bold text-sky-400">Dimas yoga</h1>
+      <small className="text-gray-400">(ID)</small><h1 className="text-2xl  text-sky-400">Dimas yoga</h1>
       </div>
       <div className="flex justify-items-center items-center">
-      <small className="text-gray-400">(JP)</small><h1 className="text-2xl font-bold text-sky-400 font-japanes">ディマスヨガ</h1>
+      <small className="text-gray-400">(JP)</small><h1 className="text-2xl  text-sky-400 mt-2 font-japanes">ディマスヨガ</h1>
       </div>
       <div className="flex justify-items-center items-center">
-      <small className="text-gray-400">(ZH)</small><h1 className="text-2xl font-bold text-sky-400">迪马斯瑜伽</h1>
+      <small className="text-gray-400">(ZH)</small><h1 className="text-2xl  text-sky-400 mt-2">迪马斯瑜伽</h1>
       </div>
       <h1 className="text-gray-400 mt-2">(hi/him)</h1>
       <div className="mt-5">
