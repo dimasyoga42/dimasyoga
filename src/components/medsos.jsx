45885da0e0eb4b5fa34c2fdfd89b069eab6a882c
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-28 13:38:19 +0700

    style: change the size of the logo

--- src/components/medsos.jsx
+++ src/components/medsos.jsx
@@ -2,17 +2,17 @@ import { GitHub, Instagram, Facebook } from 'react-feather';
 
 function Medsos() {
   return (
-    <div className='flex space-x-3 mt-5'>
+    <div className='flex space-x-3 mt-9'>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
-        <GitHub color="grey" size={40} />
+        <GitHub color="grey" size={30} />
         <small>github</small>
         </div>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
-        <Instagram color="grey" size={40} />
+        <Instagram color="grey" size={30} />
         <small>Instagram</small>
         </div>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
-        <Facebook color="grey" size={40} />
+        <Facebook color="grey" size={30} />
         <small>Facebook</small>
         </div>
     </div>
