cfb0e46f76ba4686870e68277dc85d290da5709f
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-28 19:51:33 +0700

    add link media sosial

--- src/components/medsos.jsx
+++ src/components/medsos.jsx
@@ -1,19 +1,20 @@
 import { GitHub, Instagram, Facebook } from 'react-feather';
+import { Link } from 'react-router-dom';
 
 function Medsos() {
   return (
     <div className='flex space-x-3 mt-9'>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
         <GitHub color="grey" size={30} />
-        <small>github</small>
+        <small className='cursor-pointer'><Link to="https://github.com/dimasyoga42">GitHub</Link></small>
         </div>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
         <Instagram color="grey" size={30} />
-        <small>Instagram</small>
+        <small className='cursor-pointer'><Link to="https://www.instagram.com/dimasyoga_42/profilecard/?igsh=a3RqbTBsN280Yjl4">Instagram</Link></small>
         </div>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
         <Facebook color="grey" size={30} />
-        <small>Facebook</small>
+        <small className='cursor-pointer'><Link>Facebook</Link></small>
         </div>
     </div>
   )
