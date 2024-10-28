b6ed9252861ecae3cc00dfc29adf403265aa84a0
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-28 10:19:07 +0700

    feat:  change color

--- src/components/medsos.jsx
+++ src/components/medsos.jsx
@@ -4,15 +4,15 @@ function Medsos() {
   return (
     <div className='flex space-x-3 mt-5'>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
-        <GitHub color="black" size={40} />
+        <GitHub color="grey" size={40} />
         <small>github</small>
         </div>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
-        <Instagram color="black" size={40} />
+        <Instagram color="grey" size={40} />
         <small>Instagram</small>
         </div>
         <div className='flex flex-col justify-center justify-items-center content-center items-center'>
-        <Facebook color="black" size={40} />
+        <Facebook color="grey" size={40} />
         <small>Facebook</small>
         </div>
     </div>
