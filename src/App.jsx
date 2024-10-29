64759ff6d28b3215ff5e7e8125b98aebdeeb38cb
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-29 09:56:26 +0700

    refector: path router dom untuk halaman about

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Home from './pages/home';
 import { Hero } from './components/hero';
+import About from './pages/about';
 
 function App() {
   
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
       </Routes>
      </Router>
     </div>
