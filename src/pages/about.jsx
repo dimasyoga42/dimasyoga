2875268907de6e2fe6139e400218650c0cea2b6f
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-29 09:55:43 +0700

    add: halaman about

---
+++ src/pages/about.jsx
@@ -0,0 +1,124 @@
+
+function About() {
+  return (
+    <>
+    <div className="mt-5">
+        <h1 className="font-fira font-bold underline underline-offset-1">introduction:</h1>
+        <p className="font-fira text-sm pl-2 mt-1">hello, let me introduce myself, my name is dimas yoga kurniawan, i am usually called dimas. I come from Indonesia, specifically in Pringsewu Lampung, a province in Sumatra, I am 19 years old and currently I am still studying for the first semester at a university in Lampung. I have a hobby of reading, watching films, writing and writing code, I am very open to new things whether it s learning something or making new friends. contact me if you are interested hehehe.</p>
+        </div>
+        <div className="mt-5">
+            <h1 className="font-fira  font-bold underline underline-offset-1 ">education:</h1>
+            <small className="font-fira italic text-gray-400 ">my educational journey to date</small>
+           <div className="font-fira mt-2">
+           <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+  <li>
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5">
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd" />
+      </svg>
+    </div>
+    <div className="timeline-start mb-10 md:text-end">
+      <time className="font-mono italic">2005</time>
+      <div className="text-lg font-black">I was born</div>
+      I was born in Penawar Jaya on October 8 2005
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5">
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd" />
+      </svg>
+    </div>
+    <div className="timeline-end mb-5">
+      <time className="font-mono italic">2012</time>
+      <div className="text-lg font-black">SD NEGRI 1 PENAWAR REJO</div>
+      I received basic education for 6 years at the SD NEGRI 1 PENAWAR REJO
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5">
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd" />
+      </svg>
+    </div>
+    <div className="timeline-start mb-10 md:text-end">
+      <time className="font-mono italic">2018</time>
+      <div className="text-lg font-black">SMP NEGRI 1 BANJAR MARGO</div>
+      I received junior secondary education at SMP NEGRI 1 BANJAR MARGO for 3 years
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5">
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd" />
+      </svg>
+    </div>
+    <div className="timeline-end mb-5">
+      <time className="font-mono italic">2021</time>
+      <div className="text-lg font-black">SMA NEGRI 1 BANJAR MARGO</div>
+      I received my senior secondary education at SMA NEGRI 1 BANJAR MARGO for 3 years
+    </div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div className="timeline-middle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="currentColor"
+        className="h-5 w-5">
+        <path
+          fillRule="evenodd"
+          d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+          clipRule="evenodd" />
+      </svg>
+    </div>
+    <div className="timeline-start mb-10 md:text-end">
+      <time className="font-mono italic">2024</time>
+      <div className="text-lg font-black">UNIVERSITAS NUHANNADIYAH PRINGSEWU</div>
+      and currently I am sitting in college
+    </div>
+  </li>
+</ul>
+           </div>
+        </div>
+        </>
+  )
+}
+
+export default About
