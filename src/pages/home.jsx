0c50b73ca644c2bc807da141c7d5186f8e69851d
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-29 21:36:58 +0700

    style: mengubah font dari fira code ke poppins

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,9 +2,9 @@ import Medsos from "../components/medsos"
 import Project from "../components/project"
 function Home() {
   return (
-    <div className="font-fira mt-5 ml-2 min-h-screen">
+    <div className="font-poppins mt-5 ml-2 min-h-screen">
       <div className="mt-9">
-        <h1 className=" text-sm md:text-md">a first semester student, whose hobby is trying new things, especially in the field of technology</h1>
+        <h1 className=" text-sm md:text-md">Hello everyone, I'm Dimas, happy to be friends with all of you</h1>
       </div>
       <Medsos />
       <Project />
