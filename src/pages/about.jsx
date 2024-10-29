b1cd2edefdbbdfd09456d7f9cdd2592563caefd6
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-29 21:37:08 +0700

    .

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,15 @@
 
 function About() {
   return (
-    <>
+    <div className="ml-2 font-poppins">
     <div className="mt-5">
-        <h1 className="font-fira font-bold underline underline-offset-1">introduction:</h1>
-        <p className="font-fira text-sm pl-2 mt-1">hello, let me introduce myself, my name is dimas yoga kurniawan, i am usually called dimas. I come from Indonesia, specifically in Pringsewu Lampung, a province in Sumatra, I am 19 years old and currently I am still studying for the first semester at a university in Lampung. I have a hobby of reading, watching films, writing and writing code, I am very open to new things whether it s learning something or making new friends. contact me if you are interested hehehe.</p>
+        <h1 className="font- font-bold underline underline-offset-1">introduction:</h1>
+        <p className="text-sm pl-2 mt-1">hello, let me introduce myself, my name is dimas yoga kurniawan, i am usually called dimas. I come from Indonesia, specifically in Pringsewu Lampung, a province in Sumatra, I am 19 years old and currently I am still studying for the first semester at a university in Lampung. I have a hobby of reading, watching films, writing and writing code, I am very open to new things whether it s learning something or making new friends. contact me if you are interested hehehe.</p>
         </div>
         <div className="mt-5">
-            <h1 className="font-fira  font-bold underline underline-offset-1 ">education:</h1>
-            <small className="font-fira italic text-gray-400 ">my educational journey to date</small>
-           <div className="font-fira mt-2">
+            <h1 className="font-bold underline underline-offset-1 ">education:</h1>
+            <small className=" italic text-gray-400 ">my educational journey to date</small>
+           <div className=" mt-2 text-sm">
            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
   <li>
     <div className="timeline-middle">
@@ -24,9 +24,9 @@ function About() {
           clipRule="evenodd" />
       </svg>
     </div>
-    <div className="timeline-start mb-10 md:text-end">
-      <time className="font-mono italic">2005</time>
-      <div className="text-lg font-black">I was born</div>
+    <div className="timeline-start mb-10 md:text-end text-md">
+      <time className=" italic text-gray-400">2005</time>
+      <div className="font-bold">I was born</div>
       I was born in Penawar Jaya on October 8 2005
     </div>
     <hr />
@@ -46,8 +46,8 @@ function About() {
       </svg>
     </div>
     <div className="timeline-end mb-5">
-      <time className="font-mono italic">2012</time>
-      <div className="text-lg font-black">SD NEGRI 1 PENAWAR REJO</div>
+      <time className=" italic text-gray-400">2012</time>
+      <div className=" font-bold">SD NEGRI 1 PENAWAR REJO</div>
       I received basic education for 6 years at the SD NEGRI 1 PENAWAR REJO
     </div>
     <hr />
@@ -67,8 +67,8 @@ function About() {
       </svg>
     </div>
     <div className="timeline-start mb-10 md:text-end">
-      <time className="font-mono italic">2018</time>
-      <div className="text-lg font-black">SMP NEGRI 1 BANJAR MARGO</div>
+      <time className="italic">2018</time>
+      <div className=" font-bold">SMP NEGRI 1 BANJAR MARGO</div>
       I received junior secondary education at SMP NEGRI 1 BANJAR MARGO for 3 years
     </div>
     <hr />
@@ -88,8 +88,8 @@ function About() {
       </svg>
     </div>
     <div className="timeline-end mb-5">
-      <time className="font-mono italic">2021</time>
-      <div className="text-lg font-black">SMA NEGRI 1 BANJAR MARGO</div>
+      <time className=" italic">2021</time>
+      <div className="font-bold">SMA NEGRI 1 BANJAR MARGO</div>
       I received my senior secondary education at SMA NEGRI 1 BANJAR MARGO for 3 years
     </div>
     <hr />
@@ -109,15 +109,15 @@ function About() {
       </svg>
     </div>
     <div className="timeline-start mb-10 md:text-end">
-      <time className="font-mono italic">2024</time>
-      <div className="text-lg font-black">UNIVERSITAS NUHANNADIYAH PRINGSEWU</div>
+      <time className=" italic">2024</time>
+      <div className="font-bold">UNIVERSITAS NUHANNADIYAH PRINGSEWU</div>
       and currently I am sitting in college
     </div>
   </li>
 </ul>
            </div>
         </div>
-        </>
+        </div>
   )
 }
 
