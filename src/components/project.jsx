97ae0cfdb1d97a9a7cdf7fc76376ee02e7516251
dimasyoga42 <dimasyoga9090@gmail.com>
2024-10-29 21:36:12 +0700

    style: memperbaiki ukuran font pada timline.

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -3,10 +3,10 @@ function Project() {
   return (
     <>
     <div className="mt-9">
-    <h1 className="mt-2 font-fira font-bold text-sky-400 underline underline-offset-2">Project</h1>
-    <small className="text-gray-400 italic font-fira">some things I ve been working on</small>
+    <h1 className="mt-2  font-bold text-sky-400 underline underline-offset-2">Project</h1>
+    <small className="text-gray-400 italic ">some things I ve been working on</small>
     </div>
-    <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-2 ">
+    <div className="w-full grid grid-cols-1 gap-4 lg:grid-cols-2 ">
     <div className="rounded-lg p-4 text-accent shadow-md transition-shadow duration-300 hover:shadow-lg h-32 flex justify-items-center items-center">
   <div className="flex justify-items-center items-center">
    <div>
@@ -19,7 +19,7 @@ function Project() {
 </div>
 
 <div className="rounded-lg p-4 text-accent shadow-md transition-shadow duration-300 hover:shadow-lg h-32  flex justify-items-center items-center">
-  <div className=" flex justify-items-center items-center m-auto">
+  <div className=" flex justify-items-center items-center">
     <div>
     <h2 className="font-bold  cursor-pointer underline underline-offset-2 text-sky-400">Quranku</h2>
     <p className="text-gray-400 text-xs">a website that I developed only with HTML, CSS and JavaScript</p>
