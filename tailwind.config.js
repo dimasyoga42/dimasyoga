892c8fdb8cf959c1e419f38f3d6ab84a6db3722e
raitomizuki <dimasyoga8787@gmail.com>
2025-01-14 15:05:55 +0700

    add: tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textprimry: "#030712",
+        background: "#22d3ee",
+        textscondery: "#374151",
+        hover: "#06b6d4",
+      },
+    },
   },
   plugins: [],
-}
-
+};
