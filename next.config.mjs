7fb3bfc28f37b494b2781b84ab38694b53a8cedb
dimasyoga42 <dimasyoga9090@gmail.com>
2026-04-26 12:54:56 +0700

    perbaikan animasi

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,11 @@
 const nextConfig = {
   /* config options here */
   allowedDevOrigins: ["dimasyoga.my.id"],
-  devIndicators: {
-    buildActivity: false,
-    buildActivityPosition: "bottom-right",
-  },
+  devIndicators:
+    false |
+    {
+      position: "bottom-right" | "bottom-left" | "top-right" | "top-left", // defaults to 'bottom-left',
+    },
 };
 
 export default nextConfig;
